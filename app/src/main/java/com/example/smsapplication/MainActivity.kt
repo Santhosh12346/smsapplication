55b86dd044058800fc7a23dcb833ad302c90fb06
settsanthosh <sunnysetty007@gmail.com>
2022-06-01 16:35:11 +0530

    with number

--- app/src/main/java/com/example/smsapplication/MainActivity.kt
+++ app/src/main/java/com/example/smsapplication/MainActivity.kt
@@ -18,13 +18,23 @@ class MainActivity : AppCompatActivity() {
         val  uriSms: Uri = Uri.parse("content://sms/inbox")
         val dataCursor : Cursor ?= contentResolver.query(uriSms, null, null, null, null)
 
-        var from = arrayOf("body")
-        var to = intArrayOf(android.R.id.text1)
-        var adapter = SimpleCursorAdapter(this,
+        //var from = arrayOf("body")
+        //var to = intArrayOf(android.R.id.text1)
+        var from = arrayOf("address","body")
+        var to = intArrayOf(android.R.id.text2 , android.R.id.text1)
+
+       /* var adapter = SimpleCursorAdapter(this,
             android.R.layout.simple_list_item_1,
              dataCursor,
-               from,to)
+               from,to)*/
+        var adapter = SimpleCursorAdapter(this,
+            //android.R.layout.simple_list_item_1, //layout of each row of listview
+            android.R.layout.simple_list_item_2, //layout of each row of listview
+            dataCursor, //data
+            from, //names of the columns in db table
+            to)
          var dataListView : ListView = findViewById(R.id.cpListv)
         dataListView.adapter = adapter
     }
+
 }
